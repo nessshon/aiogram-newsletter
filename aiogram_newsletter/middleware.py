54d5d4e11a88a513a31c025bfc4a6881a51df5a1
nessshon <shonness98@gmail.com>
2024-05-15 23:04:09 +0500

    Refactor middleware for private chat handling only.

--- aiogram_newsletter/middleware.py
+++ aiogram_newsletter/middleware.py
@@ -5,7 +5,7 @@ from aiogram.fsm.context import FSMContext
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from aiogram import BaseMiddleware
-from aiogram.types import TelegramObject, User
+from aiogram.types import TelegramObject, User, Chat
 
 from .manager import ANManager
 from .utils.keyboards import InlineKeyboard
@@ -30,25 +30,28 @@ class AiogramNewsletterMiddleware(BaseMiddleware):
             event: TelegramObject,
             data: Dict[str, Any],
     ) -> Any:
-        user: User = data.get("event_from_user")
-        state: FSMContext = data.get("state")
+        chat: Chat = data.get("event_chat")
 
-        state_data = await state.get_data()
-        language_code = state_data.get("language_code")
+        if chat.type == "private":
+            user: User = data.get("event_from_user")
+            state: FSMContext = data.get("state")
 
-        language_code = language_code if language_code else user.language_code
-        text_message = self.text_message or TextMessage(language_code)
-        inline_keyboard = self.inline_keyboard or InlineKeyboard(language_code)
+            state_data = await state.get_data()
+            language_code = state_data.get("language_code")
 
-        an_manager = ANManager(
-            apscheduler=self.apscheduler,
-            text_message=text_message,
-            inline_keyboard=inline_keyboard,
-            data=data,
-        )
+            language_code = language_code if language_code else user.language_code
+            text_message = self.text_message or TextMessage(language_code)
+            inline_keyboard = self.inline_keyboard or InlineKeyboard(language_code)
 
-        data["an_manager"] = an_manager
-        loop = asyncio.get_running_loop()
-        loop.__setattr__("bot", event.bot)
+            an_manager = ANManager(
+                apscheduler=self.apscheduler,
+                text_message=text_message,
+                inline_keyboard=inline_keyboard,
+                data=data,
+            )
+
+            data["an_manager"] = an_manager
+            loop = asyncio.get_running_loop()
+            loop.__setattr__("bot", event.bot)
 
         return await handler(event, data)
